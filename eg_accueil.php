3157daf6822b77cae9960237b3cd0e24ac302235
Abigail Mira <36951677+AbigailMira@users.noreply.github.com>
2018-03-13 17:56:37 +0100

    Add files via upload

---
+++ eg_accueil.php
@@ -0,0 +1,16 @@
+<!-- On commence par mettre une variable titre pour le header
+et inclure le header pour commencer la page -->
+<?php
+  $titre="accueil";
+  include("e-GET_header.php");
+?>
+
+
+<h1>e-GET</h1>
+
+
+<!-- Le contenu de la page est fini, on inclut le footer pour
+    terminer la page -->
+<?php
+  include("footer.php");
+?>
